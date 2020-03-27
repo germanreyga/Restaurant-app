07e8bfaeb7e0b976b5720fdde7e06f92b57a30b2
germanr8 <german.reyga@gmail.com>
2020-03-26 22:45:12 -0600

    Changed the title in the Index

--- restaurant/src/components/Home.js
+++ restaurant/src/components/Home.js
@@ -1,30 +1,12 @@
 import React, { Component } from "react";
-import axios from "axios";
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-
-const axiosInstance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com"
-});
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
 
 export class Home extends Component {
-  state = {
-    posts: []
-  };
-
-  componentDidMount() {
-    axiosInstance
-      .get("/posts")
-      .then(res => {
-        this.setState({ posts: res.data });
-      })
-      .catch(err => console.log(err));
-  }
-
   render() {
     return (
       <React.Fragment>
-        <h1>Restaurant App</h1>
+        <h1>Food Factory</h1>
         <AwesomeSlider>
           <div data-src={require("../bg.jpg")} />
           <div data-src={require("../ffcups.jpg")} />
@@ -33,18 +15,4 @@ export class Home extends Component {
       </React.Fragment>
     );
   }
-}
-
-function Comment(props) {
-  return (
-    <ul>
-      {props.posts.map((value, index) => {
-        return (
-          <li key={index}>
-            {value.userId}, {value.id}, {value.title}
-          </li>
-        );
-      })}
-    </ul>
-  );
 }
