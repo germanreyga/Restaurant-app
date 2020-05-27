6bd21db1b28acbb5ba975dc113c22eeed48df688
German <german.reyga@gmail.com>
2020-05-26 21:45:31 -0500

    Updates

---
+++ precache-manifest.f86fe44d4a6a2e3611cbf98b3dfc5dc6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f941246c75a00406df4d171113c2c1e",
+    "url": "/Restaurant-app/index.html"
+  },
+  {
+    "revision": "634dedd3483c1cd63f30",
+    "url": "/Restaurant-app/static/css/2.189d040b.chunk.css"
+  },
+  {
+    "revision": "53533efa200e802f00b1",
+    "url": "/Restaurant-app/static/css/main.ce8ee7f3.chunk.css"
+  },
+  {
+    "revision": "634dedd3483c1cd63f30",
+    "url": "/Restaurant-app/static/js/2.c7bc207e.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/Restaurant-app/static/js/2.c7bc207e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53533efa200e802f00b1",
+    "url": "/Restaurant-app/static/js/main.ff642f4f.chunk.js"
+  },
+  {
+    "revision": "5348d7772d4bbbed7703",
+    "url": "/Restaurant-app/static/js/runtime-main.ebd0141d.js"
+  },
+  {
+    "revision": "1ca0fcb95d1eaeb9ba32ee32adacb79b",
+    "url": "/Restaurant-app/static/media/bg.1ca0fcb9.jpg"
+  },
+  {
+    "revision": "c537065912fa5a6a32ab180e375c18c9",
+    "url": "/Restaurant-app/static/media/ffcups.c5370659.jpg"
+  },
+  {
+    "revision": "be8a9e40e2b0edefff9b12f5d4ed84e6",
+    "url": "/Restaurant-app/static/media/ffeat.be8a9e40.jpg"
+  },
+  {
+    "revision": "5d66479f49addc30bd72f77b8579531f",
+    "url": "/Restaurant-app/static/media/food.5d66479f.png"
+  },
+  {
+    "revision": "3a5c55a11499d2e3ba28883007f69971",
+    "url": "/Restaurant-app/static/media/healthy.3a5c55a1.png"
+  },
+  {
+    "revision": "5352530254bda44703d1e6e15a5e0f32",
+    "url": "/Restaurant-app/static/media/slider-1.53525302.jpg"
+  },
+  {
+    "revision": "75b04a01fca97036955a7c83f3751230",
+    "url": "/Restaurant-app/static/media/slider-7.75b04a01.jpg"
+  },
+  {
+    "revision": "38ab412fe0d3df791b20de47484613e0",
+    "url": "/Restaurant-app/static/media/smoothie.38ab412f.png"
+  }
+]);
