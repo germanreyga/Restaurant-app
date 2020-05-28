2f8175b3a5514efc7fb7279b1d27d41444910db7
German <german.reyga@gmail.com>
2020-05-27 22:16:33 -0500

    Edited object properties for react compat

--- restaurant/src/components/Order.js
+++ restaurant/src/components/Order.js
@@ -349,10 +349,10 @@ function Cart(props) {
                 <iframe
                   width="100%"
                   height="300px"
-                  frameborder="0"
+                  frameBorder="0"
                   scrolling="no"
-                  marginheight="0"
-                  marginwidth="0"
+                  marginHeight="0"
+                  marginWidth="0"
                   src={url}
                   title="Mapa waze"
                 ></iframe>
